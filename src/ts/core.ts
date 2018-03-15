d6fade55c78d425fdbdfc2e45e56eb24f90ff65c
KMishik <k.mishik@gmail.com>
2018-03-15 18:57:02 +0500

    First release services block.

--- src/ts/core.ts
+++ src/ts/core.ts
@@ -1,31 +1,92 @@
 
+interface ElementPositionXY {
+  top: number,
+  left: number,
+  height: number,
+  width?: number,
+}
+
 (function($) {
   $(function() {
 
+    // Function return JS object that has top and left position of HTML element on page
+    function getElemenyPosition(elem: HTMLElement): ElementPositionXY {
+
+      // get element rectangle
+      let elemRectangle: ClientRect = elem.getBoundingClientRect();
+
+      // get page scroll < modern || use with prev version IE8 || use when DOCTYPE set incorrect >   
+      let scrollTop: number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+      let scrollLeft: number = window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft;
+
+      // IE element may offsets relatively of left-top edge, if it does this then get this offset.   
+      let clientTop: number = document.documentElement.clientTop || document.body.clientTop || 0;
+      let clientLeft: number = document.documentElement.clientLeft || document.body.clientLeft || 0;
+
+      // Calculate element's top and left
+      let top: number = elemRectangle.top + scrollTop - clientTop;
+      let left: number = elemRectangle.left + scrollLeft - clientLeft;
+
+      return {
+        top: top,
+        left: left,
+        height: elemRectangle.height,
+        width: elemRectangle.width,
+      }
+    }
+
     $('#services .content .col .descriptor').on('click', function (event: JQuery.Event) {
       
-      let annotId: string;
-      let toFindAnnotId: string;
-      let curElem: JQuery, targetAnnotation: JQuery;
-      let curAnnotation: Node;
-      let posElem: JQuery.Coordinates, posAnnot: JQuery.Coordinates;
-
-
-      curElem = $(this);
-      annotId = curElem.data("annot-id");
+      let curElem: JQuery = $(this);
+      let annotId: string = curElem.data("annot-id");
       if (annotId === undefined || annotId === "" || isNaN(parseInt(annotId))) {
         return;
       }
-      toFindAnnotId = '#annot-' + annotId;
-      curAnnotation = $('.annotations').children(toFindAnnotId)[0];
+      let toFindAnnotId: string = '#annot-' + annotId;
+      let curAnnotation: HTMLElement = $('.annotations').children(toFindAnnotId)[0];
       if (curAnnotation === undefined) {
         return;
       }
-      targetAnnotation = $(curAnnotation);
-      posElem = curElem.offset();
-      posAnnot = { 
-        top: posElem.top + curElem.height() - 300,
-        left: posElem.left,
+      let targetAnnotation: JQuery = $(curAnnotation);
+      let posElem: ElementPositionXY = getElemenyPosition(this);
+
+      let scrollViewHeight: number = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+      );
+
+      let scrollViewWidth: number = Math.max(
+        document.body.scrollWidth, document.documentElement.scrollWidth,
+        document.body.offsetWidth, document.documentElement.offsetWidth,
+        document.body.clientWidth, document.documentElement.clientWidth
+      );
+
+      let annotCssWidth: number = parseInt(targetAnnotation.css('width'));
+      let annotCssHeight: number = parseInt(targetAnnotation.css('height'));
+      
+      // Base calculation of Top and Left position for annotation pop-up window
+      let calcTop: number = posElem.top + posElem.height - annotCssHeight;
+      let calcLeft: number = ( posElem.left >= scrollViewWidth/2 ) ?  posElem.left - annotCssWidth + posElem.width : posElem.left;
+      
+      // Precision calculation of Top position for annotation pop-up window
+      if (annotCssHeight >= document.documentElement.clientHeight || calcTop <= window.pageYOffset) {
+        calcTop = window.pageYOffset;
+      } else if (calcTop + annotCssHeight > document.documentElement.clientHeight + window.pageYOffset) {
+        calcTop = document.documentElement.clientHeight - annotCssHeight + window.pageYOffset;
+      }
+
+      // Precision calculation of Left position for annotation pop-up window
+      if (annotCssWidth >= document.documentElement.clientWidth) {
+        calcLeft = 0;
+      } else if (calcLeft + annotCssWidth > document.documentElement.clientWidth + window.pageXOffset) {
+        calcLeft = document.documentElement.clientWidth - annotCssWidth + window.pageXOffset;
+      }
+
+      let posAnnot: ElementPositionXY = { 
+        top: calcTop,
+        left: calcLeft,
+        height: annotCssWidth,
       };
 
       $('#overlay').one('click', function (event: JQuery.Event) {
@@ -37,7 +98,7 @@
 
       targetAnnotation.css({top: posAnnot.top, left: posAnnot.left});
       targetAnnotation.addClass('show');
-      
+
       event.preventDefault();
     });
 
--- static/js/core.js
+++ static/js/core.js
@@ -1,26 +1,55 @@
 (function ($) {
     $(function () {
+        function getElemenyPosition(elem) {
+            var elemRectangle = elem.getBoundingClientRect();
+            var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+            var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft;
+            var clientTop = document.documentElement.clientTop || document.body.clientTop || 0;
+            var clientLeft = document.documentElement.clientLeft || document.body.clientLeft || 0;
+            var top = elemRectangle.top + scrollTop - clientTop;
+            var left = elemRectangle.left + scrollLeft - clientLeft;
+            return {
+                top: top,
+                left: left,
+                height: elemRectangle.height,
+                width: elemRectangle.width
+            };
+        }
         $('#services .content .col .descriptor').on('click', function (event) {
-            var annotId;
-            var toFindAnnotId;
-            var curElem, targetAnnotation;
-            var curAnnotation;
-            var posElem, posAnnot;
-            curElem = $(this);
-            annotId = curElem.data("annot-id");
+            var curElem = $(this);
+            var annotId = curElem.data("annot-id");
             if (annotId === undefined || annotId === "" || isNaN(parseInt(annotId))) {
                 return;
             }
-            toFindAnnotId = '#annot-' + annotId;
-            curAnnotation = $('.annotations').children(toFindAnnotId)[0];
+            var toFindAnnotId = '#annot-' + annotId;
+            var curAnnotation = $('.annotations').children(toFindAnnotId)[0];
             if (curAnnotation === undefined) {
                 return;
             }
-            targetAnnotation = $(curAnnotation);
-            posElem = curElem.offset();
-            posAnnot = {
-                top: posElem.top + curElem.height() - 300,
-                left: posElem.left
+            var targetAnnotation = $(curAnnotation);
+            var posElem = getElemenyPosition(this);
+            var scrollViewHeight = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight);
+            var scrollViewWidth = Math.max(document.body.scrollWidth, document.documentElement.scrollWidth, document.body.offsetWidth, document.documentElement.offsetWidth, document.body.clientWidth, document.documentElement.clientWidth);
+            var annotCssWidth = parseInt(targetAnnotation.css('width'));
+            var annotCssHeight = parseInt(targetAnnotation.css('height'));
+            var calcTop = posElem.top + posElem.height - annotCssHeight;
+            var calcLeft = (posElem.left >= scrollViewWidth / 2) ? posElem.left - annotCssWidth + posElem.width : posElem.left;
+            if (annotCssHeight >= document.documentElement.clientHeight || calcTop <= window.pageYOffset) {
+                calcTop = window.pageYOffset;
+            }
+            else if (calcTop + annotCssHeight > document.documentElement.clientHeight + window.pageYOffset) {
+                calcTop = document.documentElement.clientHeight - annotCssHeight + window.pageYOffset;
+            }
+            if (annotCssWidth >= document.documentElement.clientWidth) {
+                calcLeft = 0;
+            }
+            else if (calcLeft + annotCssWidth > document.documentElement.clientWidth + window.pageXOffset) {
+                calcLeft = document.documentElement.clientWidth - annotCssWidth + window.pageXOffset;
+            }
+            var posAnnot = {
+                top: calcTop,
+                left: calcLeft,
+                height: annotCssWidth
             };
             $('#overlay').one('click', function (event) {
                 targetAnnotation.removeClass('show');
