a91c5d814e5a78d0e277d1d372e9348f9b5dbf44
KMishik <k.mishik@gmail.com>
2018-05-03 17:41:42 +0500

    Canonical location files in app.

--- aksioma/settings.py
+++ aksioma/settings.py
@@ -14,8 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates');
-STATIC_DIR = os.path.join(BASE_DIR, 'static');
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'aksioma.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATICFILES_DIRS = [STATIC_DIR, ];
+# STATICFILES_DIRS = [STATIC_DIR, ]
+# os.path.join(BASE_DIR, 'main/static/main'),
+
 
 STATIC_URL = '/static/'
--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render;
+from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse;
+# from django.http import HttpResponse
+
 
 def index(request):
-  context_dict = {};
-  return render(request, 'main/index.html', context=context_dict);
-  
+  context_dict = {}
+  return render(request, 'main/index.html', context=context_dict)
+
